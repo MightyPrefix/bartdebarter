84570dcb73b22400855ad59f15fabcbffe083c0b
Tjeef <zjef96@hotmail.com>
2024-02-28 15:14:38 +0100

    chore(db): setup basic sqlite

---
+++ db/create_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+# Connect to SQLite database (create if it doesn't exist)
+conn = sqlite3.connect("./db/posts.db")
+c = conn.cursor()
+
+# Create table if not exists
+c.execute(
+    """CREATE TABLE IF NOT EXISTS posts
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              source TEXT,
+              image_url TEXT,
+              timestamp DATETIME)"""
+)
+
+# Save (commit) the changes
+conn.commit()
+
+# Close the connection
+conn.close()
